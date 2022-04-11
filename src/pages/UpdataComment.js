df530edd9b5db98238a3b86a86e8cc0eafcbe3a1
LongGoll <luuhoanglong21@gmail.com>
2022-04-11 23:02:56 +0700

    add

--- src/pages/UpdataComment.js
+++ src/pages/UpdataComment.js
@@ -5,13 +5,17 @@ import { useNavigate } from "react-router-dom";
 function UpdataComment() {
 
     const navigate = useNavigate()
-    const [Loading, setLoading] = useState(true)
     const [inputComment, setinputComment] = useState('')
 
     //Updata Comment
     const UpdataComment = () => {
+        //đoc cookie
+        const cookieValue = document.cookie
+            .split('; ')
+            .find(row => row.startsWith('accessToken='))
+            .split('=')[1];
         var myHeaders = new Headers();
-        myHeaders.append("token", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MjQ3ZTJiZWIxZWUyODlkOTkwZTdiNjciLCJlbWFpbCI6Imx1dWhvYW5nbG9uZzIxQGdtYWlsLmNvbSIsInJvbGUiOiJhZG1pbiIsIm5hbWUiOiJBZG1pbiBMb25nIiwiZGVwYXJ0bWVudCI6IklUIiwiaWF0IjoxNjQ5Njg5MzUwLCJleHAiOjE2NDk3NzU3NTB9.7Z0-0__Kr4Ocn2_KG8EwpgS51heDSYBGVwJLfY6xe7s");
+        myHeaders.append("token", cookieValue);
         myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
         var urlencoded = new URLSearchParams();
@@ -28,9 +32,9 @@ function UpdataComment() {
         fetch(`${apiUrl}/updata-comment/${id[2]}`, requestOptions)
             .then(response => response.json())
             .then(result => {
-                if(result.success){
+                if (result.success) {
                     navigate(-1);
-                }else{
+                } else {
                     alert(result.message)
                 }
             })
@@ -38,20 +42,14 @@ function UpdataComment() {
     }
 
     let body
-    if (Loading) {
-        body = (
-            <div>
-                <div>UpData</div>
-                <label>Comment</label>
-                <input type='text' onChange={e => setinputComment(e.target.value)}></input>
-                <button onClick={UpdataComment} >Xac Nhat</button>
-            </div>
-        )
-    } else {
-        body = (
-            <div>Loading...</div>
-        )
-    }
+    body = (
+        <div>
+            <div>UpData</div>
+            <label>Comment</label>
+            <input type='text' onChange={e => setinputComment(e.target.value)}></input>
+            <button onClick={UpdataComment} >Xac Nhat</button>
+        </div>
+    )
 
     return (body)
 }
